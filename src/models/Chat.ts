61761c36c30753f3c83c3ca41143820dadac4eee
Yehia Sakr <yehiasakr4676@gmail.com>
2024-08-28 14:56:35 +0300

    Create Chat Model

---
+++ src/models/Chat.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+
+interface IMessage {
+    sender: mongoose.Schema.Types.ObjectId;
+    content: string;
+    timestamp: Date;
+}
+
+interface IChat extends Document {
+    participants: mongoose.Schema.Types.ObjectId[];
+    messages: IMessage[];
+}
+
+const messageSchema: Schema<IMessage> = new Schema({
+    sender: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now }
+});
+
+const chatSchema: Schema<IChat> = new Schema({
+    participants: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+    messages: [messageSchema]
+});
+
+
+const Chat = mongoose.model<IChat>('Chat', chatSchema);
+
+export default Chat;
