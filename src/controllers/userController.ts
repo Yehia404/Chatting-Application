2df3c560b3f1962944ab6b8bcd6d39a796ef0558
Yehia Sakr <yehiasakr4676@gmail.com>
2024-08-28 14:56:46 +0300

    Create User Controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import User from '../models/User';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const registerUser = async (req: Request, res: Response) => {
+    try {
+        const { firstname, lastname, username, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ firstname,lastname, username, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ message: 'User registered' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error registering user', error });
+    }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: 'Error logging in', error });
+    }
+};
